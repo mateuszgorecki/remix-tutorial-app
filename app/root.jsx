defa60b96d4cec95a6833baf28e99fa384346d84
mateusz <mateusz.gorecki.dev@gmail.com>
2023-01-02 13:41:08 +0100

    add styles, sass script, and new components

--- app/root.jsx
+++ app/root.jsx
@@ -5,27 +5,37 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+
+import styles from '~/styles/main.css'
+import MainNavigation from './components/MainNavigation'
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
 }
